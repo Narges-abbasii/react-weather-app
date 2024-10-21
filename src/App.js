f85a26d3c63b00be9da569b7c7f23f0b90a74350
Narges Abbasi <nargesabbasi2976@gmail.com>
2024-10-21 11:43:57 +0330

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/Narges-abbasii" target="_blank">
-          Narges Abbasi
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Narges-abbasii/react-weather-app"
-          target="_blank"
-        >
-          open-sourced in Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Narges-abbasii"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Narges Abbasi
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Narges-abbasii/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced in Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
