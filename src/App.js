6944ccb3d6e2f6720b5a1d9b331bfe5a65b647b1
Narges Abbasi <nargesabbasi2976@gmail.com>
2024-10-21 11:24:55 +0330

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Narges-abbasii" target="_blank">
+          Narges Abbasi
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Narges-abbasii/react-weather-app"
+          target="_blank"
+        >
+          open-sourced in Github
+        </a>
+      </footer>
+    </div>
+  );
 }
